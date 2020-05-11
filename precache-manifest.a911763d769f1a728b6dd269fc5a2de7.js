cecca4beccc3e9c618230445c63371b75c45808b
Agung0905 <firmansya15agung09@gmail.com>
2020-05-11 20:43:13 +0700

    menyalin isi folder build

---
+++ precache-manifest.a911763d769f1a728b6dd269fc5a2de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac952e0b15240f59c130",
+    "url": "/static/css/main.f14d0dc0.chunk.css"
+  },
+  {
+    "revision": "ac952e0b15240f59c130",
+    "url": "/static/js/main.ac952e0b.chunk.js"
+  },
+  {
+    "revision": "a1a7095c2e8878b4d395",
+    "url": "/static/js/1.a1a7095c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "99d32f35d745369b81de98e879f7d9ae",
+    "url": "/index.html"
+  }
+];
